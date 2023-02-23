467827a28fcb7d44726286fc2ee1a1f5ed4d7587
Hanna Dzierwa <hanndzi314@student.polsl.pl>
2023-02-23 21:32:00 +0100

    init backend

---
+++ blog-backend/blogJourney/src/main/java/pl/blog/blogJourney/BlogJourneyApplication.java
@@ -0,0 +1,13 @@
+package pl.blog.blogJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJourneyApplication.class, args);
+	}
+
+}
---
+++ blog-backend/blogJourney/src/test/java/pl/blog/blogJourney/BlogJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.blog.blogJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
