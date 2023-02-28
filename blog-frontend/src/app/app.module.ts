d2fa927fee7a0e55ac690874b8fc426ac06679ac
Hanna Dzierwa <hanndzi314@student.polsl.pl>
2023-02-28 21:57:59 +0100

    before big change

--- blog-frontend/src/app/app.module.ts
+++ blog-frontend/src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutComponent } from './about/about.component';
+import { DiscoverJourneyComponent } from './discover-journey/discover-journey.component';
+import { AppRoutingModule } from "./app-routing.module";
+import {SlickCarouselModule } from 'ngx-slick-carousel'
+import {MatTabsModule} from "@angular/material/tabs";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    AboutComponent,
+    DiscoverJourneyComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SlickCarouselModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
